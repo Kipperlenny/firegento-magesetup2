885287c6e42aeed24cc33382e5e8e53ea1e4d175
Kipperlenny <kontakt@webzap.eu>
2017-11-13 23:18:32 +0100

    Update configurable.js
    No idea what $t is - but the rest should work like this

--- view/frontend/web/js/configurable.js
+++ view/frontend/web/js/configurable.js
@@ -1,23 +1,40 @@
+/**
+ * Copyright © 2017 FireGento e.V.
+ * See LICENSE.md bundled with this module for license details.
+ */
 define([
-    'jquery'
-], function ($) {
+        'jquery',
+        'mage/template',
+        'Magento_Catalog/js/price-utils'
+        ], function ($, mageTemplate, priceUtils) {
     'use strict';
 
     return function (target) {
-        /**
-         * @mod Change the logic so that 'undefined' optionIds are left showing
-         */
-        target.prototype._displayRegularPriceBlock = function (optionId) {
-            if (typeof optionId == 'undefined'
-                || this.options.spConfig.optionPrices[optionId].oldPrice.amount
-                != this.options.spConfig.optionPrices[optionId].finalPrice.amount
-            ) {
-                $(this.options.slyOldPriceSelector).show();
-            } else {
-                $(this.options.slyOldPriceSelector).hide();
-            }
-        };
+        target.prototype._displayTierPriceBlock = function (optionId) {
+            var options, tierPriceHtml;
 
+            if (typeof optionId != 'undefined' &&
+                this.options.spConfig.optionPrices[optionId].tierPrices != []
+            ) {
+                options = this.options.spConfig.optionPrices[optionId];
+
+                if (this.options.tierPriceTemplate) {
+                    tierPriceHtml = mageTemplate(this.options.tierPriceTemplate, {
+                        'tierPrices': options.tierPrices,
+//                        '$t': $t,
+                        'currencyFormat': this.options.spConfig.currencyFormat,
+                        'priceUtils': priceUtils
+                    });
+                    $(this.options.tierPriceBlockSelector).html(tierPriceHtml).show();
+                    //show the tax
+//                    $(this.options.tierPriceBlockSelector).parent().next('.price-details').show();
+                }
+            } else {
+                $(this.options.tierPriceBlockSelector).hide();
+                // hide the tax also
+                $(this.options.tierPriceBlockSelector).parent().next('.price-details').hide();
+            }
+        }
         return target;
     };
 });
